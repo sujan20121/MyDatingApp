abe162004e77e25150339c6f89458045c2333b5f
Sujan Bhattarai <emailsujanbhattarai@gmail.com>
2019-09-03 23:20:03 -0500

    Initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):
+            base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
+
---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public Value()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
